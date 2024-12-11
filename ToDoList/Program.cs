2592acd68a5ce6842b0aff024881a9fe535325f5
Alex Jake <bliss.on.heart.01@outlook.com>
2024-12-11 19:14:33 +0300

    feat: create basic structure for todo list and todo item

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var todoList = new ToDoList();
+            todoList.AddToDo("Task #1");
+            todoList.AddToDo("Task #2");
+            todoList.AddToDo("Task #3");
+        }
+    }
+
+    public class ToDoList
+    {
+        private List<ToDoItem> _todos;
+
+        public ToDoList()
+        {
+            _todos = new List<ToDoItem>();
+        }
+
+        public void AddToDo(string description)
+        {
+            _todos.Add(new ToDoItem(description));
+        }
+    }
+
+    public class ToDoItem
+    {
+        public string Description { get; set; }
+
+        public ToDoItem(string description)
+        {
+            Description = description;
         }
     }
 }
